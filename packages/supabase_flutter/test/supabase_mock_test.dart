1ccffd1440a706ef14f97a49b0d174876615c69b
fueripe-desu <fueripedesu2020@outlook.com>
2023-12-01 20:21:22 -0300

    test(supabase_flutter): update initialization test with SupabaseTest class

--- packages/supabase_flutter/test/supabase_mock_test.dart
+++ packages/supabase_flutter/test/supabase_mock_test.dart
@@ -2,8 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
+  setUpAll(() async {
+    SupabaseTest.initialize();
+  });
   test('should initialize and close without timeout', () async {
-    final client = SupabaseClient('', '');
+    final client = SupabaseTest.getClient();
     await client.dispose();
   });
 }
