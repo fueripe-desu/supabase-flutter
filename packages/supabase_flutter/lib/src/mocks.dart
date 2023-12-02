572a40efe1cc66192dc9d05fc87283f9789d4eea
fueripe-desu <fueripedesu2020@outlook.com>
2023-12-02 18:06:32 -0300

    feat(supabase_flutter): add schema validation to the SupabaseTest class

--- packages/supabase_flutter/lib/src/mocks.dart
+++ packages/supabase_flutter/lib/src/mocks.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: constant_identifier_names
 
+import 'dart:convert';
+
+import 'package:collection/collection.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:http/http.dart' as http;
 
@@ -15,8 +18,9 @@ class SupabaseTest {
 
   static late String _urlString;
   static late String _urlAnon;
-  static late Map<String, Map<String, dynamic>> _schemas;
+  static late Map<String, Map<String, _ValidatorFunction>> _schemas;
   static late Map<String, List<Map<String, dynamic>>> _tables;
+  static late _JsonValidator _validator;
   static bool _initialized = false;
 
   static Future<void> initialize() async {
@@ -24,14 +28,27 @@ class SupabaseTest {
     _urlAnon = TEST_ANON;
     _schemas = {};
     _tables = {};
+    _validator = _JsonValidator();
     _initialized = true;
   }
 
+  static bool get tablesIsEmpty => _schemas.isEmpty;
+
   static void createTable(
     String tableName,
-    Map<String, dynamic> schema,
+    Map<String, _ValidatorFunction> schema,
   ) {
-    _createTable(tableName, schema);
+    if (tableExists(tableName)) {
+      throw Exception('Table already exists.');
+    }
+
+    if (schema.isEmpty) {
+      throw Exception('Schema must not be empty.');
+    }
+
+    _schemas[tableName] = {};
+    _schemas[tableName] = schema;
+    _tables[tableName] = [];
   }
 
   static List<String> getTableNames() {
@@ -44,15 +61,40 @@ class SupabaseTest {
 
   static List<Map<String, dynamic>> getTable(String tableName) {
     if (tableExists(tableName)) {
-      return _tables[tableName]!;
+      return [..._tables[tableName]!];
     } else {
       throw Exception('Table does not exist.');
     }
   }
 
+  static void insertData(
+    String tableName,
+    List<Map<String, dynamic>> contents,
+  ) {
+    _insertData(tableName, contents);
+  }
+
+  static void insertDataFromJson(String tableName, String jsonString) {
+    final parsedList = json.decode(jsonString) as List<dynamic>;
+    final contents = parsedList
+        .map((dynamic item) =>
+            Map<String, dynamic>.from(item as Map<String, dynamic>))
+        .toList();
+
+    _insertData(tableName, contents);
+  }
+
   static void clear() {
     _schemas.clear();
     _tables.clear();
+  }
+
+  static void clearTable(String tableName) {
+    if (tableExists(tableName)) {
+      _tables[tableName]!.clear();
+    } else {
+      throw Exception('Table does not exist.');
+    }
   }
 
   static MockSupabaseClient getClient() {
@@ -67,16 +109,22 @@ class SupabaseTest {
     return MockSupabaseClient(supabaseClient, httpClient);
   }
 
-  static void _createTable(
+  static void _insertData(
     String tableName,
-    Map<String, dynamic> schema,
+    List<Map<String, dynamic>> contents,
   ) {
-    if (tableExists(tableName)) {
-      throw Exception('Table already exists.');
+    if (!tableExists(tableName)) {
+      throw Exception('Table does not exist.');
     }
 
-    _schemas[tableName] = {};
-    _schemas[tableName] = schema;
+    final schema = _schemas[tableName]!;
+    final isValid = _validator.validateAll(schema, contents);
+
+    if (!isValid) {
+      throw Exception('Data does not match table schema');
+    }
+
+    _tables[tableName]!.addAll(contents);
   }
 }
 
@@ -98,9 +146,15 @@ class MockSupabaseClient {
   }
 }
 
-_ValidatorFunction sType<T>() {
-  if (T is DateTime) {
+_ValidatorFunction sType<T>({bool isNullable = false}) {
+  if (T == DateTime) {
     return (dynamic value) {
+      if (T == Null) {
+        if (isNullable) {
+          return true;
+        }
+      }
+
       final dtString = value as String;
       final dt = DateTime.tryParse(dtString);
 
@@ -108,21 +162,13 @@ _ValidatorFunction sType<T>() {
     };
   }
 
-  if (T is DateTime?) {
-    return (dynamic value) {
-      final dtString = value as String?;
-
-      if (dtString == null) {
+  return (dynamic value) {
+    if (T == Null) {
+      if (isNullable) {
         return true;
       }
+    }
 
-      final dt = DateTime.tryParse(dtString);
-
-      return dt != null;
-    };
-  }
-
-  return (dynamic value) {
     return value is T;
   };
 }
@@ -142,6 +188,21 @@ class _JsonValidator {
 
     if (!isValueValid) {
       return false;
+    }
+
+    return true;
+  }
+
+  bool validateAll(
+    Map<String, _ValidatorFunction> schema,
+    List<Map<String, dynamic>> jsonList,
+  ) {
+    for (final content in jsonList) {
+      final result = validate(schema, content);
+
+      if (result == false) {
+        return false;
+      }
     }
 
     return true;
@@ -186,6 +247,16 @@ The error was caused by the schema field: $key''',
       if (result == false) {
         return false;
       }
+    }
+
+    final deepEq = const DeepCollectionEquality.unordered().equals;
+
+    final keysList = keys.toList();
+    final schemaKeys = schema.keys.toList();
+
+    final containAllKeys = deepEq(keysList, schemaKeys);
+    if (!containAllKeys) {
+      return false;
     }
 
     return true;
