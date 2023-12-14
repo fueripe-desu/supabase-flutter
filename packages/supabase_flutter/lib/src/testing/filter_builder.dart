9e1f39bacb985af255bbfc3b20fd1bf5533ca85b
fueripe-desu <fueripedesu2020@outlook.com>
2023-12-13 22:35:44 -0300

    feat(supabase_flutter): add FilterBuilder and the equality filter

---
+++ packages/supabase_flutter/lib/src/testing/filter_builder.dart
@@ -0,0 +1,13 @@
+class FilterBuilder {
+  final List<Map<String, dynamic>> _data;
+  const FilterBuilder(List<Map<String, dynamic>> data) : _data = data;
+
+  List<Map<String, dynamic>> execute() => _data;
+
+  FilterBuilder eq(String column, dynamic value) {
+    final data = _data;
+    final newData = data.where((element) => element[column] == value).toList();
+
+    return FilterBuilder(newData);
+  }
+}
