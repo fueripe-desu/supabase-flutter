0e4996be1b4e2046cdb248764159131eeed8fd4f
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-10 08:11:02 -0300

    test(supabase_flutter): add tests for integer data types getters

---
+++ packages/supabase_flutter/test/testing/core/data_types/integer_data_types/getters/integer_test.dart
@@ -0,0 +1,81 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/integer_data_types.dart';
+
+void main() {
+  group('bitCount getter', () {
+    test('should return the maximum amount of bits', () {
+      final value = Integer(22);
+      expect(value.bitCount, 32);
+    });
+  });
+
+  group('bitLength getter', () {
+    test('should return the minimum amount of bits required', () {
+      final value = Integer(22);
+      expect(value.bitLength, 5);
+    });
+
+    test('should not count the sign bit', () {
+      final value = Integer(-1);
+      expect(value.bitLength, 0);
+    });
+  });
+
+  group('sign getter', () {
+    test('should return 1 if value is positive', () {
+      final value = Integer(22);
+      expect(value.sign, 1);
+    });
+
+    test('should return 10 if value is zero', () {
+      final value = Integer(0);
+      expect(value.sign, 0);
+    });
+
+    test('should return -1 if value is negative', () {
+      final value = Integer(-10);
+      expect(value.sign, -1);
+    });
+  });
+
+  group('isEven getter', () {
+    test('should return true if value is even', () {
+      final value = Integer(22);
+      expect(value.isEven, true);
+    });
+
+    test('should return false if value is odd', () {
+      final value = Integer(21);
+      expect(value.isEven, false);
+    });
+  });
+
+  group('isOdd getter', () {
+    test('should return true if value is odd', () {
+      final value = Integer(21);
+      expect(value.isOdd, true);
+    });
+
+    test('should return false if value is even', () {
+      final value = Integer(22);
+      expect(value.isOdd, false);
+    });
+  });
+
+  group('isNegative', () {
+    test('should return false if value is positive', () {
+      final value = Integer(22);
+      expect(value.isNegative, false);
+    });
+
+    test('should return false if value is zero', () {
+      final value = Integer(0);
+      expect(value.isNegative, false);
+    });
+
+    test('should return true if value is negative', () {
+      final value = Integer(-10);
+      expect(value.isNegative, true);
+    });
+  });
+}
---
+++ packages/supabase_flutter/test/testing/core/data_types/integer_data_types/getters/serial_test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/integer_data_types.dart';
+
+void main() {
+  group('bitCount getter', () {
+    test('should return the maximum amount of bits', () {
+      final value = Serial(22);
+      expect(value.bitCount, 32);
+    });
+  });
+
+  group('bitLength getter', () {
+    test('should return the minimum amount of bits required', () {
+      final value = Serial(22);
+      expect(value.bitLength, 5);
+    });
+  });
+
+  group('sign getter', () {
+    test('should return 1 if value is positive', () {
+      final value = Serial(22);
+      expect(value.sign, 1);
+    });
+  });
+
+  group('isEven getter', () {
+    test('should return true if value is even', () {
+      final value = Serial(22);
+      expect(value.isEven, true);
+    });
+
+    test('should return false if value is odd', () {
+      final value = Serial(21);
+      expect(value.isEven, false);
+    });
+  });
+
+  group('isOdd getter', () {
+    test('should return true if value is odd', () {
+      final value = Serial(21);
+      expect(value.isOdd, true);
+    });
+
+    test('should return false if value is even', () {
+      final value = Serial(22);
+      expect(value.isOdd, false);
+    });
+  });
+
+  group('isNegative', () {
+    test('should return false if value is positive', () {
+      final value = Serial(22);
+      expect(value.isNegative, false);
+    });
+  });
+}
