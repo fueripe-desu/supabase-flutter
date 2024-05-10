5e50f54f47da73d679e0f1a97b6758cdeabff027
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-10 08:16:06 -0300

    test(supabase_flutter): add tests for []= from integer data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/integer_data_types/index_assignment/big_serial_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/integer_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when trying to use index operator', () {
+    final value = BigSerial(1);
+    expect(() => value[0] = 3, throwsUnsupportedError);
+  });
+}
