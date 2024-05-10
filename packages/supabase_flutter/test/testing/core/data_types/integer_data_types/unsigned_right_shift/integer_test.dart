8e2b50a3cd6254faea3b73c66fd1c64a0583fb97
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-09 23:40:24 -0300

    test(supabase_flutter): add tests for >>> operator from integer data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/integer_data_types/unsigned_right_shift/integer_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/integer_data_types.dart';
+
+void main() {
+  test(
+      'should throw UnsupportedError when trying to use unsigned right shift operator',
+      () {
+    expect(() => Integer(2) >>> 3, throwsUnsupportedError);
+  });
+}
