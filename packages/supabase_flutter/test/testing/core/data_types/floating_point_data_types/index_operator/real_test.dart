c76efb2e752fc6fd8744c73407cbaf678572813d
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-13 09:10:25 -0300

    test(supabase_flutter): add tests for index operator from floating point data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/floating_point_data_types/index_operator/real_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/floating_point_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when trying to use index operator', () {
+    final value1 = Real(0);
+    expect(() => value1[1], throwsUnsupportedError);
+  });
+}
