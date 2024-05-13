d7d1aa48307cafab1e43895799a7d93719d2ab5a
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-13 09:07:54 -0300

    test(supabase_flutter): add tests for bitwise OR from floating point data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/floating_point_data_types/bitwise_or/double_precision_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/floating_point_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when trying to perform bitwise OR', () {
+    final value1 = DoublePrecision(0);
+    final value2 = DoublePrecision(1);
+    expect(() => value1 | value2, throwsUnsupportedError);
+  });
+}
