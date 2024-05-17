debf48e359e9dd483ce3ffa7897d03cd7ffeabe9
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-17 14:47:57 -0300

    test(supabase_flutter): add tests for unary minus operator from arbitrary precision data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/arbitrary_precision_data_types/unary_minus/int_decimal_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/arbitrary_precision_data_types.dart';
+
+void main() {
+  test('should inverse value when positive', () {
+    final value = Decimal(value: '-10', precision: 2, scale: 0);
+    final expected = Decimal(value: '10', precision: 2, scale: 0);
+    final operation = -value;
+    expect(operation.identicalTo(expected), true);
+  });
+
+  test('should inverse value when negative', () {
+    final value = Decimal(value: '10', precision: 2, scale: 0);
+    final expected = Decimal(value: '-10', precision: 2, scale: 0);
+    final operation = -value;
+    expect(operation.identicalTo(expected), true);
+  });
+}
