92dab5e59f3c1963cfe0b084890df597b4c27e5d
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-17 22:38:17 -0300

    test(supabase_flutter): test for abs() method from arbitrary precision data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/arbitrary_precision_data_types/methods/abs/float_numeric_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/arbitrary_precision_data_types.dart';
+
+void main() {
+  test('should return its positive counterpart if value is negative', () {
+    final value = Numeric(value: '-3.14', precision: 3, scale: 1);
+    final expected = Numeric(value: '3.14', precision: 3, scale: 1);
+    final operation = value.abs();
+    expect(operation.identicalTo(expected), true);
+  });
+
+  test('should return itself if value is already positive', () {
+    final value = Numeric(value: '3.14', precision: 3, scale: 1);
+    final expected = Numeric(value: '3.14', precision: 3, scale: 1);
+    final operation = value.abs();
+    expect(operation.identicalTo(expected), true);
+  });
+}
---
+++ packages/supabase_flutter/test/testing/core/data_types/arbitrary_precision_data_types/methods/abs/int_decimal_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/arbitrary_precision_data_types.dart';
+
+void main() {
+  test('should return its positive counterpart if value is negative', () {
+    final value = Decimal(value: '-20', precision: 2, scale: 0);
+    final expected = Decimal(value: '20', precision: 2, scale: 0);
+    final operation = value.abs();
+    expect(operation.identicalTo(expected), true);
+  });
+
+  test('should return itself if value is already positive', () {
+    final value = Decimal(value: '20', precision: 2, scale: 0);
+    final expected = Decimal(value: '20', precision: 2, scale: 0);
+    final operation = value.abs();
+    expect(operation.identicalTo(expected), true);
+  });
+}
