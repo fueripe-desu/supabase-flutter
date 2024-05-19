bd3368f817b7f2f02913a6b335886b3d2490bb75
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-19 16:15:30 -0300

    test(supabase_flutter): add tests for modulo operator from character data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/character_data_types/modulo/var_char_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/character_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when trying to perform modulo', () {
+    final value1 = VarChar('sample', length: 6);
+    final value2 = VarChar('ple', length: 3);
+    expect(() => value1 % value2, throwsUnsupportedError);
+  });
+}
