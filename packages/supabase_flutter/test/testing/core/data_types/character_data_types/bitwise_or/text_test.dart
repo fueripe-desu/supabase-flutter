b2ff004450abb383c096b621abdade11ebfc0581
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-19 16:16:37 -0300

    test(supabase_flutter): add tests for bitwise OR operator from character data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/character_data_types/bitwise_or/text_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/character_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when trying to perform bitwise OR', () {
+    final value1 = Text('sample');
+    final value2 = Text('ple');
+    expect(() => value1 | value2, throwsUnsupportedError);
+  });
+}
