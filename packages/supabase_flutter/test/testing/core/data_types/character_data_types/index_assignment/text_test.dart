6bcba728b4a3f8580651c3a1a0d21642527c4dfb
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-19 16:21:01 -0300

    test(supabase_flutter): add tests for index assignment operator from character data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/character_data_types/index_assignment/text_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/character_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when trying to perform index assignment',
+      () {
+    final value1 = Text(
+      'sample',
+    );
+    expect(() => value1[1] = 'f', throwsUnsupportedError);
+  });
+}
---
+++ packages/supabase_flutter/test/testing/core/data_types/character_data_types/index_assignment/var_char_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/character_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when trying to perform index assignment',
+      () {
+    final value1 = VarChar('sample', length: 6);
+    expect(() => value1[1] = 'f', throwsUnsupportedError);
+  });
+}
