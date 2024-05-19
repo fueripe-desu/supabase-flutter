f01c8fc274579149cc7375050817105762abba90
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-19 08:48:00 -0300

    test(supabase_flutter): add tests for bitwise AND operator from arbitrary precision data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/arbitrary_precision_data_types/bitwise_and/numeric_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/arbitrary_precision_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when using with Numeric', () {
+    final value1 = Numeric(value: '0', precision: 1, scale: 0);
+    final value2 = Numeric(value: '1', precision: 1, scale: 0);
+    expect(() => value1 & value2, throwsUnsupportedError);
+  });
+}
