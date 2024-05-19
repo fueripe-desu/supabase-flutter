a045f164585378e0828054016138755c6537ef1e
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-19 08:46:22 -0300

    test(supabase_flutter): add tests for index assignment operator from arbitrary precision data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/arbitrary_precision_data_types/index_assignment/numeric_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/arbitrary_precision_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when using with Numeric', () {
+    final value1 = Numeric(value: '0', precision: 1, scale: 0);
+    expect(() => value1[1] = 2, throwsUnsupportedError);
+  });
+}
