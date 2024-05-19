b728ec73ef03f13df7a4d8c13d9be7bb974f5d5b
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-19 16:15:17 -0300

    test(supabase_flutter): add tests for integer division operator from character data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/character_data_types/integer_division/text_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/character_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when trying to perform integer division',
+      () {
+    final value1 = Text('sample');
+    final value2 = Text('ple');
+    expect(() => value1 ~/ value2, throwsUnsupportedError);
+  });
+}
