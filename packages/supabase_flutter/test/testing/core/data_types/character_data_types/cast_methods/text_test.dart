fd7da67cecd55f319fed3749778e99c02b7f7ff4
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-19 16:17:12 -0300

    test(supabase_flutter): add tests for cast methods  from character data types

---
+++ packages/supabase_flutter/test/testing/core/data_types/character_data_types/cast_methods/text_test.dart
@@ -0,0 +1,538 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/arbitrary_precision_data_types.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/character_data_types.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/floating_point_data_types.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/integer_data_types.dart';
+
+void main() {
+  group('toString() method', () {
+    test('should correctly cast value', () {
+      final value = Text('Hello');
+      const expected = 'Hello';
+      final operation = value.toString();
+      expect(operation, expected);
+      expect(operation, isA<String>());
+    });
+  });
+
+  group('toDetailedString() method', () {
+    test('should correctly cast value', () {
+      final value = Text('Hello');
+      const expected = 'Value: \'Hello\'';
+      final operation = value.toDetailedString();
+      expect(operation, expected);
+      expect(operation, isA<String>());
+    });
+  });
+
+  group('toPrimitiveInt() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20');
+      const expected = 20;
+      final operation = value.toPrimitiveInt();
+      expect(operation, expected);
+      expect(operation, isA<int>());
+    });
+
+    test('should correctly cast zero values', () {
+      final value = Text('0');
+      const expected = 0;
+      final operation = value.toPrimitiveInt();
+      expect(operation, expected);
+      expect(operation, isA<int>());
+    });
+
+    test('should correctly cast negative values', () {
+      final value = Text('-20');
+      const expected = -20;
+      final operation = value.toPrimitiveInt();
+      expect(operation, expected);
+      expect(operation, isA<int>());
+    });
+
+    test(
+        'should throw ArgumentError when casting positive floating-point values',
+        () {
+      final value = Text('3.14');
+      expect(() => value.toPrimitiveInt(), throwsArgumentError);
+    });
+
+    test(
+        'should throw ArgumentError when casting negative floating-point values',
+        () {
+      final value = Text('-3.14');
+      expect(() => value.toPrimitiveInt(), throwsArgumentError);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toPrimitiveInt(), throwsArgumentError);
+    });
+  });
+
+  group('toPrimitiveDouble() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20.4');
+      const expected = 20.4;
+      final operation = value.toPrimitiveDouble();
+      expect(operation, expected);
+      expect(operation, isA<double>());
+    });
+
+    test('should correctly cast zero values', () {
+      final value = Text('0.0');
+      const expected = 0.0;
+      final operation = value.toPrimitiveDouble();
+      expect(operation, expected);
+      expect(operation, isA<double>());
+    });
+
+    test('should correctly cast negative values', () {
+      final value = Text('-20.4');
+      const expected = -20.4;
+      final operation = value.toPrimitiveDouble();
+      expect(operation, expected);
+      expect(operation, isA<double>());
+    });
+
+    test('should correctly cast positive int values', () {
+      final value = Text('14');
+      const expected = 14.0;
+      final operation = value.toPrimitiveDouble();
+      expect(operation, expected);
+      expect(operation, isA<double>());
+    });
+
+    test('should correctly cast negative int values', () {
+      final value = Text('-14');
+      const expected = -14.0;
+      final operation = value.toPrimitiveDouble();
+      expect(operation, expected);
+      expect(operation, isA<double>());
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toPrimitiveDouble(), throwsArgumentError);
+    });
+  });
+
+  group('toBigInteger() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20');
+      final expected = BigInteger(20);
+      final operation = value.toBigInteger();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast zero values', () {
+      final value = Text('0');
+      final expected = BigInteger(0);
+      final operation = value.toBigInteger();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast negative values', () {
+      final value = Text('-20');
+      final expected = BigInteger(-20);
+      final operation = value.toBigInteger();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test(
+        'should throw ArgumentError when casting positive floating-point values',
+        () {
+      final value = Text('3.14');
+      expect(() => value.toBigInteger(), throwsArgumentError);
+    });
+
+    test(
+        'should throw ArgumentError when casting negative floating-point values',
+        () {
+      final value = Text('-3.14');
+      expect(() => value.toBigInteger(), throwsArgumentError);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toBigInteger(), throwsArgumentError);
+    });
+  });
+
+  group('toInteger() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20');
+      final expected = Integer(20);
+      final operation = value.toInteger();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast zero values', () {
+      final value = Text('0');
+      final expected = Integer(0);
+      final operation = value.toInteger();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast negative values', () {
+      final value = Text('-20');
+      final expected = Integer(-20);
+      final operation = value.toInteger();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test(
+        'should throw ArgumentError when casting positive floating-point values',
+        () {
+      final value = Text('3.14');
+      expect(() => value.toInteger(), throwsArgumentError);
+    });
+
+    test(
+        'should throw ArgumentError when casting negative floating-point values',
+        () {
+      final value = Text('-3.14');
+      expect(() => value.toInteger(), throwsArgumentError);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toInteger(), throwsArgumentError);
+    });
+  });
+
+  group('toSmallInteger() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20');
+      final expected = SmallInteger(20);
+      final operation = value.toSmallInteger();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast zero values', () {
+      final value = Text('0');
+      final expected = SmallInteger(0);
+      final operation = value.toSmallInteger();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast negative values', () {
+      final value = Text('-20');
+      final expected = SmallInteger(-20);
+      final operation = value.toSmallInteger();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test(
+        'should throw ArgumentError when casting positive floating-point values',
+        () {
+      final value = Text('3.14');
+      expect(() => value.toSmallInteger(), throwsArgumentError);
+    });
+
+    test(
+        'should throw ArgumentError when casting negative floating-point values',
+        () {
+      final value = Text('-3.14');
+      expect(() => value.toSmallInteger(), throwsArgumentError);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toSmallInteger(), throwsArgumentError);
+    });
+  });
+
+  group('toBigSerial() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20');
+      final expected = BigSerial(20);
+      final operation = value.toBigSerial();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test(
+        'should throw ArgumentError when casting positive floating-point values',
+        () {
+      final value = Text('3.14');
+      expect(() => value.toBigSerial(), throwsArgumentError);
+    });
+
+    test(
+        'should throw ArgumentError when casting negative floating-point values',
+        () {
+      final value = Text('-3.14');
+      expect(() => value.toBigSerial(), throwsArgumentError);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toBigSerial(), throwsArgumentError);
+    });
+  });
+
+  group('toSerial() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20');
+      final expected = Serial(20);
+      final operation = value.toSerial();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test(
+        'should throw ArgumentError when casting positive floating-point values',
+        () {
+      final value = Text('3.14');
+      expect(() => value.toSerial(), throwsArgumentError);
+    });
+
+    test(
+        'should throw ArgumentError when casting negative floating-point values',
+        () {
+      final value = Text('-3.14');
+      expect(() => value.toSerial(), throwsArgumentError);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toSerial(), throwsArgumentError);
+    });
+  });
+
+  group('toSmallSerial() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20');
+      final expected = SmallSerial(20);
+      final operation = value.toSmallSerial();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test(
+        'should throw ArgumentError when casting positive floating-point values',
+        () {
+      final value = Text('3.14');
+      expect(() => value.toSmallSerial(), throwsArgumentError);
+    });
+
+    test(
+        'should throw ArgumentError when casting negative floating-point values',
+        () {
+      final value = Text('-3.14');
+      expect(() => value.toSmallSerial(), throwsArgumentError);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toSmallSerial(), throwsArgumentError);
+    });
+  });
+
+  group('toReal() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20.4');
+      final expected = Real(20.4);
+      final operation = value.toReal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast zero values', () {
+      final value = Text('0.0');
+      final expected = Real(0.0);
+      final operation = value.toReal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast negative values', () {
+      final value = Text('-20.4');
+      final expected = Real(-20.4);
+      final operation = value.toReal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast positive int values', () {
+      final value = Text('14');
+      final expected = Real(14.0);
+      final operation = value.toReal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast negative int values', () {
+      final value = Text('-14');
+      final expected = Real(-14.0);
+      final operation = value.toReal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toReal(), throwsArgumentError);
+    });
+  });
+
+  group('toDoublePrecision() method', () {
+    test('should correctly cast positive values', () {
+      final value = Text('20.4');
+      final expected = DoublePrecision(20.4);
+      final operation = value.toDoublePrecision();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast zero values', () {
+      final value = Text('0.0');
+      final expected = DoublePrecision(0.0);
+      final operation = value.toDoublePrecision();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast negative values', () {
+      final value = Text('-20.4');
+      final expected = DoublePrecision(-20.4);
+      final operation = value.toDoublePrecision();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast positive int values', () {
+      final value = Text('14');
+      final expected = DoublePrecision(14.0);
+      final operation = value.toDoublePrecision();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast negative int values', () {
+      final value = Text('-14');
+      final expected = DoublePrecision(-14.0);
+      final operation = value.toDoublePrecision();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toDoublePrecision(), throwsArgumentError);
+    });
+  });
+
+  group('toNumeric() method', () {
+    test('should correctly cast positive floating-point values', () {
+      final value = Text('20.4');
+      final expected = Numeric(value: '20.4', precision: 3, scale: 1);
+      final operation = value.toNumeric();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast floating-point zero values', () {
+      final value = Text('0.0');
+      final expected = Numeric(value: '0.0', precision: 2, scale: 1);
+      final operation = value.toNumeric();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast integer zero values', () {
+      final value = Text('0');
+      final expected = Numeric(value: '0', precision: 1, scale: 0);
+      final operation = value.toNumeric();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast floating-point negative values', () {
+      final value = Text('-20.4');
+      final expected = Numeric(value: '-20.4', precision: 3, scale: 1);
+      final operation = value.toNumeric();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast positive int values', () {
+      final value = Text('14');
+      final expected = Numeric(value: '14', precision: 2, scale: 0);
+      final operation = value.toNumeric();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast negative int values', () {
+      final value = Text('-14');
+      final expected = Numeric(value: '-14', precision: 2, scale: 0);
+      final operation = value.toNumeric();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toNumeric(), throwsArgumentError);
+    });
+  });
+
+  group('toDecimal() method', () {
+    test('should correctly cast positive floating-point values', () {
+      final value = Text('20.4');
+      final expected = Decimal(value: '20.4', precision: 3, scale: 1);
+      final operation = value.toDecimal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast floating-point zero values', () {
+      final value = Text('0.0');
+      final expected = Decimal(value: '0.0', precision: 2, scale: 1);
+      final operation = value.toDecimal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast integer zero values', () {
+      final value = Text('0');
+      final expected = Decimal(value: '0', precision: 1, scale: 0);
+      final operation = value.toDecimal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast floating-point negative values', () {
+      final value = Text('-20.4');
+      final expected = Decimal(value: '-20.4', precision: 3, scale: 1);
+      final operation = value.toDecimal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast positive int values', () {
+      final value = Text('14');
+      final expected = Decimal(value: '14', precision: 2, scale: 0);
+      final operation = value.toDecimal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should correctly cast negative int values', () {
+      final value = Text('-14');
+      final expected = Decimal(value: '-14', precision: 2, scale: 0);
+      final operation = value.toDecimal();
+      expect(operation.identicalTo(expected), true);
+    });
+
+    test('should throw ArgumentError when casting non-numerical values', () {
+      final value = Text('sample');
+      expect(() => value.toDecimal(), throwsArgumentError);
+    });
+  });
+
+  group('toChar() method', () {
+    test('should correctly cast text values', () {
+      final value = Text('sample string');
+      final expected = Char('sample string', length: 13);
+      final operation = value.toChar();
+      expect(operation.identicalTo(expected), true);
+    });
+  });
+
+  group('toVarChar() method', () {
+    test('should correctly cast text values', () {
+      final value = Text('sample string');
+      final expected = VarChar('sample string', length: 13);
+      final operation = value.toVarChar();
+      expect(operation.identicalTo(expected), true);
+    });
+  });
+
+  group('toText() method', () {
+    test('should correctly cast text values', () {
+      final value = Text('sample string');
+      final expected = Text('sample string');
+      final operation = value.toText();
+      expect(operation.identicalTo(expected), true);
+    });
+  });
+}
