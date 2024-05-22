ec4f1ec5b6802d9eab89f31add608d38d42b162f
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-22 11:39:17 -0300

    feat(supabase_flutter): add some predefined locales for use in the locale class

---
+++ packages/supabase_flutter/lib/src/testing/utils/locale_data.dart
@@ -0,0 +1,138 @@
+// ignore_for_file: constant_identifier_names
+
+enum Locales {
+  // Afrikaans (South Africa)
+  af_ZA('af_ZA'),
+  // Arabic (United Arab Emirates)
+  ar_AE('ar_AE'),
+  // Arabic (Saudi Arabia)
+  ar_SA('ar_SA'),
+  // Arabic (Egypt)
+  ar_EG('ar_EG'),
+  // Arabic (Morocco)
+  ar_MA('ar_MA'),
+  // Bulgarian (Bulgaria)
+  bg_BG('bg_BG'),
+  // Catalan (Spain)
+  ca_ES('ca_ES'),
+  // Czech (Czech Republic)
+  cs_CZ('cs_CZ'),
+  // Danish (Denmark)
+  da_DK('da_DK'),
+  // German (Austria)
+  de_AT('de_AT'),
+  // German (Switzerland)
+  de_CH('de_CH'),
+  // German (Germany)
+  de_DE('de_DE'),
+  // Greek (Greece)
+  el_GR('el_GR'),
+  // English (Australia)
+  en_AU('en_AU'),
+  // English (Canada)
+  en_CA('en_CA'),
+  // English (United Kingdom)
+  en_GB('en_GB'),
+  // English (Ireland)
+  en_IE('en_IE'),
+  // English (India)
+  en_IN('en_IN'),
+  // English (New Zealand)
+  en_NZ('en_NZ'),
+  // English (United States)
+  en_US('en_US'),
+  // English (South Africa)
+  en_ZA('en_ZA'),
+  // Spanish (Argentina)
+  es_AR('es_AR'),
+  // Spanish (Chile)
+  es_CL('es_CL'),
+  // Spanish (Colombia)
+  es_CO('es_CO'),
+  // Spanish (Spain)
+  es_ES('es_ES'),
+  // Spanish (Mexico)
+  es_MX('es_MX'),
+  // Spanish (Peru)
+  es_PE('es_PE'),
+  // Spanish (United States)
+  es_US('es_US'),
+  // Estonian (Estonia)
+  et_EE('et_EE'),
+  // Finnish (Finland)
+  fi_FI('fi_FI'),
+  // French (Belgium)
+  fr_BE('fr_BE'),
+  // French (Canada)
+  fr_CA('fr_CA'),
+  // French (Switzerland)
+  fr_CH('fr_CH'),
+  // French (France)
+  fr_FR('fr_FR'),
+  // Hebrew (Israel)
+  he_IL('he_IL'),
+  // Hindi (India)
+  hi_IN('hi_IN'),
+  // Croatian (Croatia)
+  hr_HR('hr_HR'),
+  // Hungarian (Hungary)
+  hu_HU('hu_HU'),
+  // Indonesian (Indonesia)
+  id_ID('id_ID'),
+  // Italian (Switzerland)
+  it_CH('it_CH'),
+  // Italian (Italy)
+  it_IT('it_IT'),
+  // Japanese (Japan)
+  ja_JP('ja_JP'),
+  // Korean (Korea)
+  ko_KR('ko_KR'),
+  // Lithuanian (Lithuania)
+  lt_LT('lt_LT'),
+  // Latvian (Latvia)
+  lv_LV('lv_LV'),
+  // Malay (Malaysia)
+  ms_MY('ms_MY'),
+  // Norwegian Bokmål (Norway)
+  nb_NO('nb_NO'),
+  // Dutch (Belgium)
+  nl_BE('nl_BE'),
+  // Dutch (Netherlands)
+  nl_NL('nl_NL'),
+  // Polish (Poland)
+  pl_PL('pl_PL'),
+  // Portuguese (Brazil)
+  pt_BR('pt_BR'),
+  // Portuguese (Portugal)
+  pt_PT('pt_PT'),
+  // Romanian (Romania)
+  ro_RO('ro_RO'),
+  // Russian (Russia)
+  ru_RU('ru_RU'),
+  // Slovak (Slovakia)
+  sk_SK('sk_SK'),
+  // Slovenian (Slovenia)
+  sl_SL('sl_SL'),
+  // Serbian (Serbia)
+  sr_RS('sr_RS'),
+  // Swedish (Sweden)
+  sv_SE('sv_SE'),
+  // Thai (Thailand)
+  th_TH('th_TH'),
+  // Turkish (Turkey)
+  tr_TR('tr_TR'),
+  // Ukrainian (Ukraine)
+  uk_UA('uk_UA'),
+  // Vietnamese (Vietnam)
+  vi_VN('vi_VN'),
+  // Simplified chinese (China)
+  zh_CN('zh_CN'),
+  // Traditional chinese (Hong Kong)
+  zh_HK('zh_HK'),
+  // Traditional chinese (Taiwan)
+  zh_TW('zh_TW');
+
+  const Locales(this.localeString);
+
+  final String localeString;
+}
