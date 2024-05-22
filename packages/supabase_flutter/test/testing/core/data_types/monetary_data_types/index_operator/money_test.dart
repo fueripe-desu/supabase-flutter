262446504c1c18b2727b5ea96d74b78047ffbc1e
fueripe-desu <fueripedesu2020@outlook.com>
2024-05-22 18:16:09 -0300

    test(supabase_flutter): add tests for index operator from Money type

---
+++ packages/supabase_flutter/test/testing/core/data_types/monetary_data_types/index_operator/money_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:supabase_flutter/src/testing/core/currency/currencies.dart';
+import 'package:supabase_flutter/src/testing/core/data_types/monetary_data_types.dart';
+
+void main() {
+  test('should throw UnsupportedError when performing index operator', () {
+    final value1 = Money(r'$0.00', overrideCurrency: Currencies.usd);
+    expect(() => value1[0], throwsUnsupportedError);
+  });
+}
